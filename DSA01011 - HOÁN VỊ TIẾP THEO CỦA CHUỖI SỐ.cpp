dfd5ecc53455906cf0b36a4374b998b9e08f3b06
Mphuc02 <105010427+Mphuc02@users.noreply.github.com>
2022-06-10 04:22:55 +0700

    Update DSA01011 - HOÁN VỊ TIẾP THEO CỦA CHUỖI SỐ.cpp

--- DSA01011 - HOÁN VỊ TIẾP THEO CỦA CHUỖI SỐ.cpp
+++ DSA01011 - HOÁN VỊ TIẾP THEO CỦA CHUỖI SỐ.cpp
@@ -1,47 +1,40 @@
-#include<bits/stdc++.h>
-#define mod			 1000000007
-#define ll			 long long
-#define	p(x)			 pair<x,x>
-#define v(x)			 vector<x>
-#define tree			 node*
-#define pb(a)			 push_back(a)
-#define pf(a)			 push_front(a)
-#define FOR(i,l,r)		 for(int i=l;i<r;i++)
-#define FORX(i,l,r,x)	  	 for(int i=l;i<r;i+=x)
-#define FORD(i,l,r)		 for(int i=l;i>=r;i--)
-#define correct(x,y,n,m) 0<=(x)&&(x)<(n)&&0<=(y)&&(y)<(m)
-#define cin(M,n)		 FOR(i,0,n)cin>>M[i]
-#define cout(M,n)		 FOR(i,0,n)cout<<M[i]
-#define rs(M,x)		    	 memset(M,x,sizeof(M))
-#define reset()			 FOR(i, 0, 1000001)A[i].clear(),check[i]=false
-#define faster()		 cin.tie(0); ios_base::sync_with_stdio(false); cout.tie(0);
-#define run()			 int t; cin >> t; while (t--)
-#define pq(x )			 priority_queue<x>
-#define neg_pq(x)		 priority_queue<x, vector<x>, greater<x>>
-#define all(M)			 M.begin(),M.end()
+#include<iostream>
+#include<algorithm>
 using namespace std;
 
-//_______________________T_O_A_N________________________//
-
-string Sinh(string s) {
-	FOR(i, 1, s.length())if (s[i] > s[i - 1]) {
-		next_permutation(all(s));
-		return s;
+int main()
+{
+	int n;
+	cin>>n;
+	while(n--)
+	{
+		int a,b;
+		string s;
+		cin>>a>>s;
+		b=s.length();
+		s='0'+s;
+		int idx=b,cmax=b;
+		while(s[idx]<=s[idx-1] )
+		{
+			if(s[idx]>=s[cmax])
+				cmax=idx;
+			idx--;
+		}
+		if(idx==1)
+		{
+			cout<<a<<" BIGGEST\n";
+		}
+		else
+		{
+			for(int i = idx ;i<=b;i++)
+			{
+				if(s[i]>s[idx-1] && s[i] <=s[cmax])
+					cmax=i;
+			}
+			swap(s[idx-1],s[cmax]);
+			sort(s.begin()+idx,s.end());
+			s.erase(s.begin());
+			cout<<a<<" "<<s<<endl;
+		}
 	}
-	return "BIGGEST";
 }
-
-int main() {
-	faster();
-	run() {
-		int a; cin >> a;
-		string s; cin >> s;
-		cout << a << " ";
-		cout << Sinh(s) << endl;
-	}
-
-
-}
-
-
-//_______________________T_O_A_N________________________//
