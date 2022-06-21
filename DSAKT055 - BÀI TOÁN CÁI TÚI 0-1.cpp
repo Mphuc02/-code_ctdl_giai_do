ea0fd5c76325c9ab6b54fae119b7973b4e49cef7
Mphuc02 <105010427+Mphuc02@users.noreply.github.com>
2022-06-21 21:57:19 +0700

    Update DSAKT055 - BÀI TOÁN CÁI TÚI 0-1.cpp

--- DSAKT055 - BÀI TOÁN CÁI TÚI 0-1.cpp
+++ DSAKT055 - BÀI TOÁN CÁI TÚI 0-1.cpp
@@ -1,24 +1,35 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-int main() {
-	int t; cin >> t;
-	while (t--) {
-		int n, k; cin >> n >> k;
-		vector<pair<int, int>> M(n + 1);
-		for (int i = 0; i < n; i++)cin >> M[i].first;
-		for (int i = 0; i < n; i++)cin >> M[i].second;
-		int dp[1001][1001];
-		for (int i = 0; i <= n; i++) {
-			for (int j = 0; j <= k; j++) {
-				if (i == 0 || j == 0)dp[i][j] = 0;
-				else {
-					if (j < M[i].first)dp[i][j] = dp[i - 1][j];
-					else dp[i][j] = max(dp[i - 1][j], M[i].second + dp[i - 1][j - M[i].first]);
-				}
+struct ct
+{
+    int x,y;
+};
 
-			}
-		}
-		cout << dp[n][k] << endl;
-	}
+int main()
+{
+    int n;
+    cin>>n;
+    while(n--)
+    {
+        int a,b;
+        cin>>a>>b;
+        int f[a+1][b+1]={};
+        vector<ct>ds(a+1);
+        for(int i = 1;i<=a;i++)
+            cin>>ds[i].x;
+        for(int i = 1;i<=a;i++)
+            cin>>ds[i].y;
+        for(int i = 1;i<=a;i++)
+        {
+            for(int j = 1;j<=b;j++)
+            {
+                if(j<ds[i].x)
+                    f[i][j]=f[i-1][j];
+                else
+                    f[i][j]=max(f[i-1][j],f[i-1][j-ds[i].x]+ds[i].y);
+            }
+        }
+        cout<<f[a][b]<<endl;
+    }
 }
